5a7a17c00d7582cdc0e483a3ac3bbe34176259da
Geoffrey <geoffrey@moveparallel.com>
2022-12-13 09:49:13 -0800

    Apply `args-file` feature to updated fork (new base) (#8)
    Apply changes from #7

--- src/input.ts
+++ src/input.ts
@@ -5,11 +5,13 @@ import stringArgv from "string-argv";
 export interface Input {
     toolchain: string | undefined;
     args: string[];
+    argsFilePath: string | undefined;
     useCross: boolean;
 }
 
 export function get(): Input {
     let toolchain: string = input.getInput("toolchain");
+    const argsFilePath: string = input.getInput("args-file");
 
     if (toolchain.startsWith("+")) {
         toolchain = toolchain.slice(1);
@@ -17,6 +19,7 @@ export function get(): Input {
 
     return {
         args: stringArgv(input.getInput("args")),
+        argsFilePath: "" !== argsFilePath ? argsFilePath : undefined,
         useCross: input.getInputBool("use-cross"),
         toolchain: "" !== toolchain ? toolchain : undefined,
     };
